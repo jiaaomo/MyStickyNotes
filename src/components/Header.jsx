e26149b36d06d002b416206a5bb8237a138ff3bd
Jiaao Mo <49225707+jiaaomo@users.noreply.github.com>
2021-07-07 13:24:47 -0700

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EventNoteIcon />
+        Sticky Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
